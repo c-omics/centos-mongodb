d8ada2904c1969afd2d17c292220096222b05d10
Ian Merrick <MerrickI@Cardiff.ac.uk>
2019-04-07 20:36:13 +0100

    Added a simple test for CI

---
+++ 7/tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# docker run -i bigr.bios.cf.ac.uk:4567/comics/grafana bash < tests.sh
+#
+
+# test ############
+uname -a
+cat /etc/os-release
+mongod --version
+###################
+
+
